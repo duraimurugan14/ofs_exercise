aa514c806d27220cd65a941738e5a3d4c7a65756
duraimurugan_rajendran <duraimurugan.rajendran@object-frontier.com>
2014-10-21 17:17:08 +0530

    my changes

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-function pageRedirect(){
-	alert("Into other function");
-	// window.location = "http://www.google.com/";
-    //var url = "http://www.google.com/";
-   // window.open("http://www.google.com/", "_self");
-    window.location.href = 'http://www.google.com/';
-    alert("test");
+
+"use strict";
+
+var isUserValidated = false;
+
+function validateUserName(userName, userPassword) {
+	if(userName.length > 8){
+		alert("User Name cannot exceed 8 characters");
+		isUserValidated = false;
+	}
+	else if(userName === "admin12"){
+		if (userPassword < 6) {
+			alert("Password you entered is too short!");
+			isUserValidated = false;
+		}
+		else if (userPassword === "admin22") {
+			alert("You are logged in!");	
+			isUserValidated = true;
+		}
+	}
+	else{
+		alert("The user credentials you entered is invalid. Please check and re-enter");
+		isUserValidated = false;
+	}
+	return isUserValidated;
 }
 
-function validateCredentials(user, pass) { 
-	var userName1 = user.value;
-	var passwordCheck1 = pass.value; 
-	pageRedirect();
+
+function validateCredentials(userNameData, passwordData) { 
+	var name = userNameData.value;
+	var password = passwordData.value; 
+	validateUserName(name, password);
+	alert(isUserValidated);
 }
+
 
 
