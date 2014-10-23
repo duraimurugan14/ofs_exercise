af8868a50975a08ac458f2b3e0120acd60890039
duraimurugan_rajendran <duraimurugan.rajendran@object-frontier.com>
2014-10-23 17:22:21 +0530

    third commit

--- feeds.js
+++ feeds.js
@@ -1,1 +1,17 @@
 //feeds.js
+//document.body.onload = pushFeedsIntoMemory;
+
+createFeed
+
+function createFeed(feedsText) { 
+  // create a new div element 
+  // and give it some content 
+  alert(feedsText.value);
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode("Hi there and greetings!"); 
+  newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+}
